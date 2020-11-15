e6d40f8c020db5d4f0acadd4c2b8f135d6683e11
bellakratchei <isabellavk@hotmail.com.br>
2020-11-14 23:25:38 -0300

    primeiro commit da aplicação em C Sharp/.NET

---
+++ BellaKratchei.PrimeiroTesteMVC/BellaKratchei.PrimeiroTesteMVC.Web/Controllers/TesteController.cs
@@ -0,0 +1,24 @@
+﻿using BellaKratchei.PrimeiroTesteMVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BellaKratchei.PrimeiroTesteMVC.Web.Controllers
+{
+    public class TesteController : Controller
+    {
+        // O método é na verdade uma Action que pode ser chamada via URL
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            return View("Saudacao", pessoa);
+        }
+    }
+}
---
+++ BellaKratchei.PrimeiroTesteMVC/BellaKratchei.PrimeiroTesteMVC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BellaKratchei.PrimeiroTesteMVC.Web.MvcApplication" Language="C#" %>
